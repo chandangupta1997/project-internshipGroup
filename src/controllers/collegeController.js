8c8cb137ff55bc3bfde2879a9887628de2555265
chandangupta1997 <chandangupta8000@gmail.com>
2022-03-23 10:42:18 +0530

    alomostdone

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -26,6 +26,28 @@ const isValidRequestBody=function(requestBody){
 
 
 
+// const createCollege = async function( req , res ) {
+//     try {
+//         const data = req.body
+
+//         // check : if request body is empty
+//         if ( !Object.keys(data) > 0)  return res.status(400).send({status : false ,message : "Please enter data"})
+
+//         // check : if any data field has no value or it is empty
+//         if( !isValid(data.name) )    return res.status(400).send({ status : false, message: 'please provide short name'})
+//         if( !isValid(data.fullname) )    return res.status(400).send({ status : false, message: 'please provide fullname'})
+//         if( !isValid(data.logoLink) )    return res.status(400).send({ status : false, message: 'please provide logoLink'})
+
+//         // Create :  Intern 
+//         const createdIntern = await collegeModel.create(data)
+//         return res.status(201).send({ status : true , data : createdIntern})
+//     }
+//     catch ( error ) {
+//         console.log(error.message)
+//         return res.status(500).send({ status : false,message : error.message})
+//     }
+// }
+
 
 
 
@@ -63,6 +85,9 @@ const createCollege = async function(req,res){
     if(!isValid(fullName)){res.status(400).send("College Full name is REquired ")
            return }
 
+    // if( !isValid(data.logoLink) )   
+    // return res.status(400).send({ status : false, message: 'please provide logoLink'})
+
 
     //validation ends 
 
@@ -86,11 +111,25 @@ const createCollege = async function(req,res){
 
 const getCollege =async function(req,res){
 
+
+
+    try{
+
+        
+
     let data =req.query
-    if(!data)return res.send("please enter data in query")
+    if(!isValidRequestBody(data)){
+        res.status(400).send({status:false,msg:"Invalid request parameters Please Provide query to search "})
+        return
+
+    }
+    
 
     let collegeName = req.query.name
-    if(!collegeName) res.status().send("please senter")
+    //if( !isValid(collegeName) )    return res.status(400).send({ status : false, message: 'please provide college Name'})
+    //if(!collegeName) res.status().send("please senter")
+
+
     let collegeDetails=await collegeModel.findOne({name:collegeName})
 
     // yha empty array aa rha hai isliye  phle find tha sirf  
@@ -98,14 +137,45 @@ const getCollege =async function(req,res){
 
     if(!collegeDetails)return res.status(404).send({status:"false",msg:"no such College found check college id "})
 
-    res.status(200).send({status:"true",msg:collegeDetails})
-    let collegeId = collegeDetails.id  //.select({_id:1})
-    console.log(collegeId)
+   // res.status(200).send({status:"true",msg:collegeDetails})
+
+
+    const collegeId = await collegeModel.find({ name : collegeName , isDeleted : false }).select({ _id : 1})
+    if( !Object.keys(collegeId).length > 0)  return res.status(404).send({ status : false, message: 'No data found'})
+
+    // let collegeId = collegeDetails.id  //.select({_id:1})
+    // console.log(collegeId)
 
     // //let collegeId =collegeDetails.name //or full name 
     // //phir 
+
+
+
+
+    let colleges = await collegeModel.findById(collegeId).select({ name:1 , fullname:1 , logoLink:1 , _id:0})
+
+
     let collegeInterns= await internModel.find({_collegeid:collegeId})
     console.log(collegeInterns)
+
+    let obj = {name : colleges.name ,
+        fullname : colleges.fullname ,
+        logoLink : colleges.logoLink , 
+        interests : collegeInterns}
+
+//  send : response
+      res.status(200).send({status : true ,data : obj })
+
+
+    }
+
+
+
+    catch(error){
+
+        res.status(500).send(error.message)
+
+    }
 
 }
 
